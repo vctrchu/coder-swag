559e0804734ca86271a6f3b7a001b5bf49e65a93
victorchu8 <victorchu1996@gmail.com>
2019-07-21 18:54:25 -0600

    lesson-03 complete

---
+++ coder-swag/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by VICTOR CHU on 2019-07-21.
+//  Copyright © 2019 Victor Chu. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
